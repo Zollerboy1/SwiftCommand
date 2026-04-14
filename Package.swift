0d3141bd7b335735e3ce194274bb8c7bc4b8b86e
Josef Zoller <josef@walterzollerpiano.com>
2026-04-14 17:04:01 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -12,13 +12,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftCommand",
-            targets: ["SwiftCommand"]),
+            targets: ["SwiftCommand"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-system", from: "1.6.1"),
+        .package(
+            url: "https://github.com/apple/swift-async-algorithms",
+            from: "1.0.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +30,10 @@ let package = Package(
         .target(
             name: "SwiftCommand",
             dependencies: [
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+                .product(
+                    name: "AsyncAlgorithms",
+                    package: "swift-async-algorithms"
+                ),
                 .product(name: "SystemPackage", package: "swift-system"),
             ]
         ),
@@ -34,7 +41,10 @@ let package = Package(
             name: "SwiftCommandTests",
             dependencies: [
                 "SwiftCommand",
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+                .product(
+                    name: "AsyncAlgorithms",
+                    package: "swift-async-algorithms"
+                ),
             ]
         ),
     ]
